70fa333fda03f84a1176a56d8a99f9ea78e6229e
Jen Merritt <jenjomez@gmail.com>
2020-01-08 17:13:57 +0000

    journey end locations passed

--- models/traveller.js
+++ models/traveller.js
@@ -7,6 +7,7 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation);
 
 };
 
