2749b706baac2765e876d3bcb366d5135f558d46
Jen Merritt <jenjomez@gmail.com>
2020-01-08 17:29:56 +0000

    distance tests passed

--- models/traveller.js
+++ models/traveller.js
@@ -16,11 +16,13 @@ Traveller.prototype.getJourneysByTransport = function (transport) {
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journey => journey.distance > minDistance);
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal + journey.distance;
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
